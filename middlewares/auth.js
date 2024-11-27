43802a1c4882890a0700fd862a2bee3f382afed0
UMASANGAR Kirishanth <71499827+Kirishankerpro@users.noreply.github.com>
2024-11-27 14:48:44 +0100

    lock

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,17 +1,18 @@
-const UnauthorizedError = require("../errors/unauthorized");
-const jwt = require("jsonwebtoken");
-const config = require("../config");
+const jwt = require('jsonwebtoken')
 
-module.exports = (req, res, next) => {
+const UnauthorizedError = require('../errors/unauthorized')
+const config = require('../config')
+
+module.exports = (req, __res, next) => {
   try {
-    const token = req.headers["x-access-token"];
+    const token = req.headers['x-access-token']
     if (!token) {
-      throw "not token";
+      throw 'not token'
     }
-    const decoded = jwt.verify(token, config.secretJwtToken);
-    req.user = decoded;
-    next();
+    const decoded = jwt.verify(token, config.secretJwtToken)
+    req.user = decoded
+    next()
   } catch (message) {
-    next(new UnauthorizedError(message));
+    next(new UnauthorizedError(message))
   }
-};
+}
