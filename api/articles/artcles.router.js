b3e040cde73307a214673fe2ae7b40cb385b391b
UMASANGAR Kirishanth <71499827+Kirishankerpro@users.noreply.github.com>
2024-12-07 20:26:01 +0100

    all articles

--- api/articles/artcles.router.js
+++ api/articles/artcles.router.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const articlesController = require('./articles.controller')
 
+router.get('/', articlesController.getAll)
 router.post('/', articlesController.create)
 router.put('/:id', articlesController.update)
 router.delete('/:id', articlesController.delete)
