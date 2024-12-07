2d5fb3b1c3a4ddae44e2295ee15e3954b8fdb842
UMASANGAR Kirishanth <71499827+Kirishankerpro@users.noreply.github.com>
2024-12-07 21:49:47 +0100

    articles

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { Server } = require('socket.io')
 const cors = require('cors')
 const NotFoundError = require('./errors/not-found')
 const userRouter = require('./api/users/users.router')
-const articlesRouter = require('./api/articles/artcles.router')
+const articlesRouter = require('./api/articles/articles.router')
 const usersController = require('./api/users/users.controller')
 const authMiddleware = require('./middlewares/auth')
 require('./api/articles/articles.schema') // temporaire
